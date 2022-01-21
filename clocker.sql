c5a8236072af1bb88c56fc813a49e65fdb7a1fcc
konradbycz <konradbycz@gmail.com>
2022-01-21 23:21:10 +0100

    Changed DB schema

--- clocker.sql
+++ clocker.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Czas generowania: 17 Sty 2022, 00:49
+-- Czas generowania: 21 Sty 2022, 23:20
 -- Wersja serwera: 10.1.28-MariaDB
 -- Wersja PHP: 7.1.11
 
@@ -34,6 +34,32 @@ CREATE TABLE `client` (
   `name` varchar(50) COLLATE utf8_polish_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
 
+--
+-- Zrzut danych tabeli `client`
+--
+
+INSERT INTO `client` (`id`, `userId`, `name`) VALUES
+(1, 1, 'ZUT');
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `groups`
+--
+
+CREATE TABLE `groups` (
+  `id` int(11) NOT NULL,
+  `ownerId` int(11) NOT NULL,
+  `name` varchar(255) COLLATE utf8_polish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+
+--
+-- Zrzut danych tabeli `groups`
+--
+
+INSERT INTO `groups` (`id`, `ownerId`, `name`) VALUES
+(1, 1, 'Testowa');
+
 -- --------------------------------------------------------
 
 --
@@ -42,9 +68,18 @@ CREATE TABLE `client` (
 
 CREATE TABLE `project` (
   `id` int(11) NOT NULL,
+  `ownerId` int(11) NOT NULL,
+  `groupId` int(11) DEFAULT NULL,
   `clientId` int(11) DEFAULT NULL,
   `name` varchar(50) COLLATE utf8_polish_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+
+--
+-- Zrzut danych tabeli `project`
+--
+
+INSERT INTO `project` (`id`, `ownerId`, `groupId`, `clientId`, `name`) VALUES
+(1, 1, 1, 1, 'Projekt clocker');
 
 -- --------------------------------------------------------
 
@@ -55,7 +90,7 @@ CREATE TABLE `project` (
 CREATE TABLE `task` (
   `id` int(11) NOT NULL,
   `userId` int(11) NOT NULL,
-  `project` varchar(50) COLLATE utf8_polish_ci NOT NULL,
+  `projectId` int(11) NOT NULL,
   `name` varchar(50) COLLATE utf8_polish_ci NOT NULL,
   `start` datetime NOT NULL,
   `stop` datetime NOT NULL,
@@ -66,8 +101,8 @@ CREATE TABLE `task` (
 -- Zrzut danych tabeli `task`
 --
 
-INSERT INTO `task` (`id`, `userId`, `project`, `name`, `start`, `stop`, `description`) VALUES
-(1, 1, 'NWM', 'NWM', '2022-01-17 00:00:00', '2022-01-18 00:00:00', 'NWM');
+INSERT INTO `task` (`id`, `userId`, `projectId`, `name`, `start`, `stop`, `description`) VALUES
+(1, 2, 1, 'NWM', '2022-01-17 00:00:00', '2022-01-18 00:00:00', 'NWM');
 
 -- --------------------------------------------------------
 
@@ -89,8 +124,30 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`id`, `firstname`, `lastname`, `email`, `hash`, `role`) VALUES
-(1, 'Konrad', 'Byczynski', 'konrad@zut.pl', '$2y$10$B/i1eV8CCZaNL1QTKZPvOeKiRzA2uJ32DrlxEcRJKUY09pq3ZoPkO', 'user'),
-(2, 'Pawel', 'Drozgowski', 'pawel@zut.pl', '$2y$10$8xVc.oVBAiAJ9s/CfqA84O1ghbCoXe54n.WII0jD2umW2Hbevrvom', 'user');
+(1, 'Konrad', 'Byczynski', 'konrad@zut.pl', '$2y$10$B/i1eV8CCZaNL1QTKZPvOeKiRzA2uJ32DrlxEcRJKUY09pq3ZoPkO', 'admin'),
+(2, 'Pawel', 'Drozgowski', 'pawel@zut.pl', '$2y$10$8xVc.oVBAiAJ9s/CfqA84O1ghbCoXe54n.WII0jD2umW2Hbevrvom', 'user'),
+(4, 'maks', 'wiese', 'maks@zut.pl', '$2y$10$XZMpgUq2WDeLADmGY3D4ketQaDWHN.YdW3R3NwGVqAwSSz4ALKlC2', 'user');
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `usersgroups`
+--
+
+CREATE TABLE `usersgroups` (
+  `id` int(11) NOT NULL,
+  `groupId` int(11) NOT NULL,
+  `userId` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+
+--
+-- Zrzut danych tabeli `usersgroups`
+--
+
+INSERT INTO `usersgroups` (`id`, `groupId`, `userId`) VALUES
+(1, 1, 4),
+(2, 1, 2),
+(3, 1, 1);
 
 --
 -- Indeksy dla zrzutów tabel
@@ -100,6 +157,12 @@ INSERT INTO `user` (`id`, `firstname`, `lastname`, `email`, `hash`, `role`) VALU
 -- Indexes for table `client`
 --
 ALTER TABLE `client`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `groups`
+--
+ALTER TABLE `groups`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -122,6 +185,12 @@ ALTER TABLE `user`
   ADD UNIQUE KEY `email` (`email`);
 
 --
+-- Indexes for table `usersgroups`
+--
+ALTER TABLE `usersgroups`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -129,13 +198,19 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT dla tabeli `client`
 --
 ALTER TABLE `client`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT dla tabeli `groups`
+--
+ALTER TABLE `groups`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT dla tabeli `project`
 --
 ALTER TABLE `project`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT dla tabeli `task`
@@ -147,7 +222,13 @@ ALTER TABLE `task`
 -- AUTO_INCREMENT dla tabeli `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT dla tabeli `usersgroups`
+--
+ALTER TABLE `usersgroups`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
